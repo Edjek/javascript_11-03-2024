a2b2f34298055db1350e001d95de17d0b36758be
Edjek <edjek@hotmail.fr>
2024-03-11 17:22:06 +0100

    first day

--- js-lesson/js/07-loops.js
+++ js-lesson/js/07-loops.js
@@ -2,11 +2,23 @@
 
 // Les boucles sont des structures permettant d'executer plusieurs fois des instructions
 
-// Boucle : while(){...} (tant que)
+// Une boucle a besoin de 3 informations principales:
+// initialisation
+// condition
+// incrementation
 
+// Boucle : while(){...} (tant que)
 let i = 0;
 
 while (i < 10) {
-    i = i + 1;
     console.log(i);
+    i = i + 1;
+}
+
+i = 0;
+
+while (i < 10) {
+    // let res = i * 8;
+    console.log(`${i} x 8 = ${i * 8}`);
+    i = i + 1;
 }
