9ac75c21fe7a6047c023f6b694bceb5799dbef52
Edjek <edjek@hotmail.fr>
2024-03-12 10:53:39 +0100

    second day

--- js-lesson/js/07-loops.js
+++ js-lesson/js/07-loops.js
@@ -8,17 +8,48 @@
 // Incrementation
 
 // Boucle : while(){...} (tant que)
+// let i = 0;
+
+// while (i < 10) {
+//     console.log(i);
+//     i = i + 1;
+// }
+
+// let j = 0;
+
+// while (j < 10) {
+//     // let res = j * 8;
+//     console.log(`${j} x 9 = ${j * 9}`);
+//     j = j + 1;
+// }
+
+/*
+    La population de la ville Marrakech est de 1, 000, 000 d’habitants et elle augmente de 50, 000 habitants par an.
+    Celle de la ville Agadir est de 500, 000 habitants et elle augmente de 8% par an.
+    Ecrire un algorithme permettant de déterminer dans combien d’années la population de la ville Agadir dépassera celle de la ville Marrakech.
+
+    Afficher la phrase : Il faudra X années avant que la population d'agadir dépasse celle de Marrakech
+*/
+let marrakech = 1000000;
+let agadir = 500000;
 let i = 0;
 
-while (i < 10) {
-    console.log(i);
+while (agadir < marrakech) {
+    marrakech = marrakech + 50000;
+    agadir = agadir * 1.08;
     i = i + 1;
+    // SOLUTION SUCRE SYNTAXIQUE
+    // marrakech +=  50000;
+    // agadir *= 1.08;
+    // i++
+
+    // SUCRE SYNTAXIQUE
+    // i++ on ne peut avancer que de 1 en 1 avec cette façon
+    // La nouvelle valeur est égale à l'ancienne plus le calcul atttribué
+    // i += 1;
+    // i *= 3;
 }
 
-let j = 0;
-
-while (j < 10) {
-    // let res = j * 8;
-    console.log(`${j} x 8 = ${j * 8}`);
-    j = j + 1;
-}
+console.log(
+    `Il faudra ${i} années avant que la population d'agadir dépasse celle de Marrakech`
+);
